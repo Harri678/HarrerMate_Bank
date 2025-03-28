083b55f44dd89f5518d4d922cc30693543f81e5c
Petrik Diak <diak@petrik.hu>
2025-03-28 11:39:40 +0100

    kesz1

---
+++ BankFeladat/BankCard.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankFeladat
+{
+	internal class BankCard : BankingService
+	{
+		double accountBalance;
+		string cardNumber;
+
+		public string CardNumber { get; }
+		public double AccountBalance { get; }
+
+		public BankCard(Owner owner, double accountBalance, string cardNumber)
+		{
+			this.accountBalance = accountBalance;
+			this.cardNumber = cardNumber;
+		}
+
+		public bool Purchace(double amount)
+		{
+			if (accountBalance - amount >= 0)
+			{
+				accountBalance -= amount;
+				return true;
+			}
+			return false;
+		}
+	}
+}
