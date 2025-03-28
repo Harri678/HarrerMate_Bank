817ec387dc517f58d43324f736643b3b8f64f477
Petrik Diak <diak@petrik.hu>
2025-03-28 10:50:36 +0100

    Owner_Kesz

---
+++ BankFeladat/Owner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankFeladat
+{
+	internal sealed class Owner
+	{
+		public string Name { get; set; }
+
+	}
+
+
+}
